291284783ffa0b6aa0d24c5172a2025f04aa84ef
Dragos Apostol <dragos.apostol@kambi.com>
2021-04-22 17:22:23 +0300

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+// import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Your code goes here.
-      </header>
+      <header className="App-header">Your code goes here.</header>
     </div>
   );
 }
 
-export default App;
+const rootApp = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
+export default rootApp;
